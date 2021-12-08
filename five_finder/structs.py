3da128e64dbafe3f9d6b6dc2a9ff07daad3f6858
Danimal <chao-mu@hackpoetic.com>
2021-12-08 12:53:38 -0500

    Support chance of random heap selection

--- five_finder/structs.py
+++ five_finder/structs.py
@@ -1,10 +1,23 @@
 from five_finder.heap import Heap
 
+import random
+
+# Ideas: If an edge is involved in a path X times, drop it
+
 class ChaosHeap(Heap):
-    def __init__(self, target):
+    def __init__(self, target, chance_random=0):
         self.target = target
+        self.chance_random = chance_random
 
         super().__init__()
+
+    def get(self):
+        if random.random() < self.chance_random:
+            idx = random.randint(0, self.size - 1)
+        else:
+            idx = 0
+
+        return super().get(idx)
 
     def less_than(self, a, b):
         return abs(a[-1][0] - self.target) < abs(b[-1][0] - self.target)
